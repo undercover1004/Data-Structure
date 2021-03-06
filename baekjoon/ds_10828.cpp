686af1e0982a2981f161d8c2fb7f9a1d956b8311
Jung Min Kim <50435968+kvmii@users.noreply.github.com>
2021-03-06 15:15:00 +0900

    Create ds_10828.cpp

---
+++ baekjoon/ds_10828.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+int n;
+stack<int> stk;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string str;
+
+		cin >> str;
+		if (str == "push") {
+			int s;
+			cin >> s;
+			stk.push(s);
+		}
+
+		else if (str == "top") {
+			if (stk.empty()) {
+				cout << -1 << endl;
+			}
+			else {
+				cout << stk.top() << endl;
+			}
+		}
+
+		else if (str == "size") {
+			cout << stk.size() << endl;
+		}
+
+		else if (str == "empty") {
+			if (str.empty()) cout << 1 << endl;
+			else cout << 0 << endl;
+		}
+
+		else if (str == "pop") {
+			if (stk.empty()) cout << -1 << endl;
+			else {
+				cout << stk.top() << endl;
+				stk.pop();
+			}
+		}
+
+	}
+}
