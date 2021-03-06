1a64b6c0d787e00d568eebb1215e0959d2d9ea45
Jung Min Kim <50435968+kvmii@users.noreply.github.com>
2021-03-06 17:23:20 +0900

    Create ds_1158.cpp

---
+++ baekjoon/ds_1158.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int n, k;
+
+int main() {
+	cin >> n >> k;
+	list<int> lst;
+	// list 활용
+	for (int i = 1; i <= n; i++) {
+		lst.push_back(i);
+	}
+	
+	list<int>::iterator it = lst.begin();
+
+	cout << "<";
+	while (lst.size() != 1) {
+		for (int i = 0; i < k - 1; i++) {
+			if (it == lst.end()) {
+				it = lst.begin();
+				// 넘어가면 처음으로
+			}
+			it++;
+			if (it == lst.end()) {
+				it = lst.begin();
+				// 넘어가면 처음으로
+			}
+		}
+
+		cout << *it << ", ";
+		it = lst.erase(it);
+		if (it == lst.end()) {
+			it = lst.begin();
+		}
+	}
+
+	cout << *it << ">";
+	return 0;
+}
